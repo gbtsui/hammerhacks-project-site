f5e0764bd7bb686d5d309e667b512d8bef1c2757
Gabriel Tsui <148885745+gbtsui@users.noreply.github.com>
2024-11-23 09:22:00 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var index = 0;
+
+function changeQuote() {
+/** */
+    var quotes = [
+        `"We are cooked" - St Paisios`,
+        `"Partial faithfulness is no faithfulness at all" - St Augustine`,
+        `"Every letter of yours breathes Moabitish pride. So much can a single bull inflate a single bubble that you practically make distinguished asses into gods." - Martin Luther`,
+        `"You seem to be wrangling about goat's wool, like the man who watched the play in an empty theater." - Martin Luther`,
+        `"sigmoid, sigmoid, on the wall, which neuron is the most activated of them all?" - Gabriel Tsui`,
+        `"Placeholder Quote, I don't know, just put something there" - Eamon Kerr`
+    ]
+    var quotesWithSpeaker = [
+        ["We are cooked", "St Paisios"],
+        ["Sin is stored in the balls", "St Augustine"],
+        ["Every letter of yours breathes Moabitish pride. So much can a single bull inflate a single bubble that you practically make distinguished asses into gods.", "Martin Luther"],
+        ["You seem to be wrangling about goat's wool, like the man who watched the play in an empty theater.", "Martin Luther"],
+        ["sigmoid, sigmoid, on the wall, which neuron is the most activated of them all?", "Gabriel Tsui"]
+    ] //unused for now
+    
+    var blockquote = document.querySelector("blockquote");
+
+    index++;
+    if (index>quotes.length-1){
+        index=0
+    }
+    blockquote.innerHTML = quotes[index]
+}
